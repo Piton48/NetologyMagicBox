9d5eb73b29eceda716696cb9c6c824d5d93a85a1
Sergey Seliverstov <seliverstov243@mail.ru>
2023-02-21 22:00:31 +0300

    MagicBox is working

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,30 @@
-public class MagicBox {
+import java.util.Random;
+
+public class MagicBox<T> {
+    public int maxCapacity;
+    protected int counter = 0;
+    protected T[] items;
+
+    public MagicBox(int capacity) {
+        this.maxCapacity = capacity;
+        items = (T[]) new Object[maxCapacity];
+    }
+
+    public boolean add(T item) {
+        if (this.counter < maxCapacity) {
+            this.items[this.counter] = item;
+            counter++;
+            return true;
+        }
+        return false;
+    }
+
+    public T pick() {
+        if ((this.counter) == (this.maxCapacity)) {
+            Random random = new Random();
+            return items[random.nextInt(maxCapacity)];
+        } else {
+            throw new RuntimeException("Магическая Коробка не заполнена!");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MagicBox<String> str = new MagicBox<>(3);
+        MagicBox<Integer> num = new MagicBox<>(10);
+
+        str.add("Vanya");
+        str.add("Olya");
+        //str.add("Petya");
+
+        for (int i = 0; i < 20; i++){
+            num.add(i);
+        }
+
+        System.out.println(num.pick());
+        System.out.println(str.pick());
     }
 }
